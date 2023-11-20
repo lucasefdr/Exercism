8fbd1a67869405268e8387e787dc9a58fbae1f67
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-20 10:55:29 -0300

    CSharp - Booleans: QuestLogic

---
+++ CSharp/CSharp/Booleans/QuestLogic.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp.Boolean;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+        => !knightIsAwake;
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+        => knightIsAwake || archerIsAwake || prisonerIsAwake;
+
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+        => !archerIsAwake && prisonerIsAwake;
+
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent && !archerIsAwake) return true;
+        if (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake) return true;
+        return false;
+    }
+}
