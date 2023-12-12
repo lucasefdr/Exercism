4d16d3d087826c33d1640db91d1f36a16ff4a1b7
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-12-12 13:16:21 -0300

    CSharp - Strings: LogLine

--- CSharp/CSharp/Strings/LogLine.cs
+++ CSharp/CSharp/Strings/LogLine.cs
@@ -2,16 +2,10 @@
 
 public static class LogLine
 {
-    public static string Message(string logLine) => logLine.Split(": ")[1];
+    public static string Message(string logLine) => logLine[(logLine.IndexOf(':') + 1)..].Trim();
 
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+    public static string LogLevel(string logLine) => logLine[1..logLine.IndexOf(']')].ToLower();
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
 }
 
--- CSharp/CSharpTest/Strings/LogLineTest.cs
+++ CSharp/CSharpTest/Strings/LogLineTest.cs
@@ -6,8 +6,44 @@ namespace CSharpTest.Strings;
 public class LogLineTest
 {
     [Fact]
-    void GetMessageFromALogLine()
+    void Message_ReturnsMessageFromALogLine()
     {
-        Assert.Equal("Stack overflow", LogLine.Message("[ERROR]: Stack overflow"));
+        // ARRANGE
+        var fullMessage = "[ERROR]: Invalid operation";
+        var message = "Invalid operation";
+
+        // ACT
+        var result = LogLine.Message(fullMessage);
+
+        // ASSERT
+        Assert.Equal(message, result);
+    }
+
+    [Fact]
+    void LogLevel_ReturnsLineLogLevel()
+    {
+        // ARRANGE
+        var fullMessage = "[ERROR]: Invalid operation";
+        var logLineMessage = "error";
+
+        // ACT
+        var result = LogLine.LogLevel(fullMessage);
+
+        // ASSERT
+        Assert.Equal(logLineMessage, result);
+    }
+
+    [Fact]
+    void Reformat_ReturnsLogLineFormatted()
+    {
+        // ARRANGE 
+        var logLine = "[INFO]: Operation completed";
+        var logLineFormatted = "Operation completed (info)";
+
+        // ACT
+        var result = LogLine.Reformat(logLine);
+
+        // ASSERT
+        Assert.Equal(logLineFormatted, result);
     }
 }
