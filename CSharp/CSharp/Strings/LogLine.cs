f6694ad73dde2ba29c2ac5aa9ccdb34c0715bcab
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-12-12 08:54:30 -0300

    CSharp - Strings: LogLine

---
+++ CSharp/CSharp/Strings/LogLine.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Strings;
+
+public static class LogLine
+{
+    public static string Message(string logLine) => logLine.Split(": ")[1];
+
+    public static string LogLevel(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+    }
+
+    public static string Reformat(string logLine)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+    }
+}
+
---
+++ CSharp/CSharpTest/Strings/LogLineTest.cs
@@ -0,0 +1,13 @@
+﻿using CSharp.Strings;
+using Xunit;
+
+namespace CSharpTest.Strings;
+
+public class LogLineTest
+{
+    [Fact]
+    void GetMessageFromALogLine()
+    {
+        Assert.Equal("Stack overflow", LogLine.Message("[ERROR]: Stack overflow"));
+    }
+}
